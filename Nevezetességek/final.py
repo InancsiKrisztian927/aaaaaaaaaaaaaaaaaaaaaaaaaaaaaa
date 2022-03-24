e253cfa85e91dbe3a35eeac69e5cc64aff04dc84
InancsiKrisztian927 <35177338+InancsiKrisztian927@users.noreply.github.com>
2022-03-24 11:23:24 +0100

    Add files via upload

--- Nevezetességek/final.py
+++ Nevezetességek/final.py
@@ -41,7 +41,7 @@ while option !=-1:
             
     elif option == 2:
 
-        answer = ["azmegmicsodaaaa??!","ammeg mi?? ?","Tesó te ismerz ilyet?","Nem létezik","Öcskösöm jól tetszel vagy?","Te meg mit hiszel?","Valami létezőt XDD"]
+        answer = ["azmegmicsodaaaa??!","ammeg mi?? ?","Tesó te ismerz ilyet?","Nem létezik","Öcskösöm jól tetszel vagy?","Te meg mit hiszel?","Valami létezőt XDD","kys","Tu may giet may","XDDDDDDDDDDDDDD","Lyjasuó camp"]
         
         print()
         print("\tEgyes évszámok nem valós információt tartalmazhatnak!")
@@ -54,6 +54,7 @@ while option !=-1:
         for i in range(len(adatok)):
             
             if a == adatok[i].country:
+                
                 print("\t\n",adatok[i].rekordok())
                 found = True
                 
@@ -65,22 +66,26 @@ while option !=-1:
                 break
 
             elif a == "Szlovákia":
+                
                 print("\t\n",random.choice(answer))
                 
                 found = True
                 break
 
             elif a == "Ukrajna":
+                
                 print("\t\nMég létezik, de nem sokáig")
 
                 found = True
                 break
                 
         if found == False:
+            
             print("\t\nNincs ilyen ország az adatbázisban!")
             print()
                         
     elif option == 3:
+        
         print()
         print("\tEgyes évszámok nem valós információt tartalmazhatnak!")
         print("\t\nA kiírt információk Ország - ahhoz tartozó nevezetesség - város - építés/felfedezés/megalapítás éve -sorrendben jelenik meg!")
@@ -94,8 +99,10 @@ while option !=-1:
                 found = True
 
         if found == False:
+            
             print("\tNincs ilyen évszám az adatbázisban!")
             print()
+            
     elif option == 4:
 
         print()
